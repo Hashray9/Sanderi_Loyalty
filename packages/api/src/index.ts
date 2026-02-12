964a78def5622fa9fc5b3b3e32b5ea11bffb541f
Hashray_10 <hashraykumbhani258@gmail.com>
2026-02-12 19:14:51 +0530

    railway fix

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -36,7 +36,7 @@ app.use('/app', appRouter);
 app.use(errorHandler);
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 
   // Start scheduled jobs
